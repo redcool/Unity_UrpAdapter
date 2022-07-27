a6c79d73ee986411f8d2cde87efdf80be93644cf
ken <redcool007@qq.com>
2022-07-27 17:40:46 +0800

    add helper scripts

---
+++ Scripts/URPBaseCamera.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+#if UNITY_EDITOR
+using UnityEditor;
+[CustomEditor(typeof(URPBaseCamera))]
+public class URPBaseCameraEditor : Editor {
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        var inst = target as URPBaseCamera;
+
+        if(GUILayout.Button("Auto Set Overlay Cameras"))
+        {
+            inst.SetCameras();
+            Selection.activeGameObject = null;  
+        }
+    }
+}
+#endif
+
+public class URPBaseCamera : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SetCameras()
+    {
+        var mainCam = GetComponent<Camera>();
+        var maincamData = GetComponent<UniversalAdditionalCameraData>();
+        if (!maincamData )
+            return;
+
+        maincamData.renderType = CameraRenderType.Base;
+        maincamData.cameraStack.Clear();
+
+        var cams = FindObjectsOfType<Camera>();
+        foreach (var cam in cams)
+        {
+            if(cam == mainCam)
+                continue;
+
+            var camData = cam.GetComponent<UniversalAdditionalCameraData>();
+            if (!camData)
+            {
+                camData = cam.gameObject.AddComponent<UniversalAdditionalCameraData>();
+            }
+            camData.renderType = CameraRenderType.Overlay;
+
+            maincamData.cameraStack.Add(cam);
+        }
+
+    }
+}
